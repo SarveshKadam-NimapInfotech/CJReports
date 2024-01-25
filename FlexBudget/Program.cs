4bdbfe43fed20ca7c76ac6b5b9e2059a5b2a81b6
SarveshKadam-NimapInfotech <sarveshkadam@nimapinfotech.com>
2024-01-25 19:41:12 +0530

    FlexBudget and UtilityTracker code added

---
+++ FlexBudget/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace FlexBudget
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.FlexBudget();
+        }
+
+        public void FlexBudget()
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            excelApp.Interactive = false;
+            excelApp.DisplayAlerts = false;
+            excelApp.DisplayClipboardWindow = false;
+            excelApp.DisplayStatusBar = false;
+
+            string flexBudgetFilePath = @"C:\Users\Nimap\Documents\FlexBudget\Flex Budget 2023-11 - CJNC.xlsx";
+
+            string CjNorthFilePath = @"C:\Users\Nimap\Documents\FlexBudget\FF IS - CJ North 12.23.xlsx";
+
+            Excel.Workbook flexBudgetWorkbook = excelApp.Workbooks.Open(flexBudgetFilePath);
+
+            Excel.Workbook CjNorthWorkbook = excelApp.Workbooks.Open(CjNorthFilePath);
+
+            try
+            {
+                var date = "12/01/2023";
+
+                DateTime parsedDate = DateTime.ParseExact(date, "MM/dd/yyyy", CultureInfo.InvariantCulture);
+
+                string month = parsedDate.Month.ToString();
+
+                Worksheet RawDataPlSheet = flexBudgetWorkbook.Worksheets["Raw Data PL"];
+
+                Worksheet CjNorthSheet = CjNorthWorkbook.Worksheets[1];
+
+                Range clearRange = RawDataPlSheet.Range["B1:S" + RawDataPlSheet.Rows.Count];
+                clearRange.Clear();
+
+                Range copyRange = CjNorthSheet.Range["A1:R" + CjNorthSheet.Rows.Count];
+                copyRange.Copy(Type.Missing);
+
+                Range pasteRange = RawDataPlSheet.Range["B1:S" + RawDataPlSheet.Rows.Count];
+                pasteRange.PasteSpecial(XlPasteType.xlPasteAll);
+
+                Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+
+                int rawDataLastRow = RawDataPlSheet.Cells[RawDataPlSheet.Rows.Count, 1].End[Excel.XlDirection.xlUp].Row + 1;
+
+                for (int i = 1; i <= rawDataLastRow; i++)
+                {
+                    var cellB = RawDataPlSheet.Cells[i, 2];
+                    var cellC = RawDataPlSheet.Cells[i, 3];
+                    var cellD = RawDataPlSheet.Cells[i, 4];
+
+                    // Check if any of the cells are null
+                    if (cellB.Value != null)
+                    {
+                        string cellValueB = Convert.ToString(cellB.Value);
+                        string cellValueC = Convert.ToString(cellC?.Value);
+                        string cellValueD = Convert.ToString(cellD?.Value);
+
+                        if (cellValueB.Contains("Net Sales") || cellValueB.Contains("Labor Matrix") || cellValueB.Contains("Ideal Food Cost"))
+                        {
+                            string key = cellValueB;
+
+                            if (!dict.ContainsKey(key))
+                            {
+                                dict[key] = new List<string>();
+                            }
+
+                            dict[key].Add(cellValueC ?? "");  // Value1
+                            dict[key].Add(cellValueD ?? "");  // Value2
+                        }
+                    }
+                }
+
+
+                foreach (var kvp in dict)
+                {
+                    Console.WriteLine($"Key: {kvp.Key}");
+
+                    List<string> values = kvp.Value;
+
+                    for (int j = 0; j < values.Count; j += 2)
+                    {
+                        string value1 = values[j];
+                        string value2 = (j + 1 < values.Count) ? values[j + 1] : "N/A";
+
+                        Console.WriteLine($"  Value1: {value1}, Value2: {value2}");
+                    }
+
+                    Console.WriteLine();
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); ;
+            }
+            finally
+            {
+                excelApp.Quit();
+                Marshal.ReleaseComObject(excelApp);
+            }
+
+        }
+    }
+}
---
+++ UtilityTracker/Program.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Media;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace UtilityTracker
+{
+    public class YourEntityClass
+    {
+        public string AccountNumber { get; set; }
+        public List<EntityDataRow> DataRows { get; set; } = new List<EntityDataRow>();
+    }
+
+    public class EntityDataRow
+    {
+        public string Entity { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime BillDate { get; set; }
+        public decimal Cost { get; set; }
+        public int Usage { get; set; }
+        public string Vendor { get; set; }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.UtilityTracker();
+        
+        }
+
+        public void UtilityTracker()
+        {
+            Excel.Application excelApp = new Excel.Application();
+            excelApp.Visible = true;
+            excelApp.Interactive = false;
+            excelApp.DisplayAlerts = false;
+            excelApp.DisplayClipboardWindow = false;
+            excelApp.DisplayStatusBar = false;
+
+            string costUsageFilePath = @"C:\Users\Nimap\Documents\UtilityTracker\InputFiles\Cost Usage.xlsx";
+
+            string utilityTrackerFilePath = @"C:\Users\Nimap\Documents\UtilityTracker\InputFiles\RE - Utility Tracker (2023-09).xlsx";
+
+            //string utilityTrackerFilePath = @"C:\Users\Nimap\Documents\UtilityTracker\RE - Utility Tracker (2023-09).xlsx";
+
+
+            Excel.Workbook costUsageWorkbook = excelApp.Workbooks.Open(costUsageFilePath);
+
+            Excel.Workbook utilityTrackerWorkbook = excelApp.Workbooks.Open(utilityTrackerFilePath);
+
+            try
+            {
+                Worksheet trackerSheet = utilityTrackerWorkbook.Worksheets["Conservice tracking"];
+
+                int trackerLastRow = trackerSheet.Cells[trackerSheet.Rows.Count, 1].End[Excel.XlDirection.xlUp].Row + 1;
+
+                HashSet<string> uniqueAccountNumbers = new HashSet<string>();
+
+                for (int i = 2; i < trackerLastRow; i++)
+                {
+                    string accountNo = Convert.ToString(trackerSheet.Cells[i, 3].Value);
+                    uniqueAccountNumbers.Add(accountNo);
+                }
+
+                List<string> accountNumber = new List<string>(uniqueAccountNumbers);
+
+                //foreach (string number in accountNumber)
+                //{
+                //    Console.WriteLine(number);
+                //}
+
+                Worksheet costUsageSheet = costUsageWorkbook.Worksheets[1];
+
+                int costUsageLastRow = costUsageSheet.Cells[costUsageSheet.Rows.Count, 1].End[Excel.XlDirection.xlUp].Row + 1;
+
+                List<YourEntityClass> matchedDataList = new List<YourEntityClass>();
+
+                foreach (string accountNo in accountNumber)
+                {
+                    YourEntityClass entityData = new YourEntityClass
+                    {
+                        AccountNumber = accountNo,
+                        DataRows = new List<EntityDataRow>()
+                    };
+
+                    for (int i = 2; i < costUsageLastRow; i++)
+                    {
+                        string dataAccountNo = Convert.ToString(costUsageSheet.Cells[i, 1].Value);
+
+                        if (accountNo == dataAccountNo)
+                        {
+                            EntityDataRow rowData = new EntityDataRow
+                            {
+                                Entity = Convert.ToString(costUsageSheet.Cells[i, 4].Value),  
+                                StartDate = Convert.ToDateTime(costUsageSheet.Cells[i, 6].Value),  
+                                EndDate = Convert.ToDateTime(costUsageSheet.Cells[i, 7].Value),    
+                                BillDate = Convert.ToDateTime(costUsageSheet.Cells[i, 8].Value),   
+                                Cost = Convert.ToDecimal(costUsageSheet.Cells[i, 10].Value),       
+                                Usage = Convert.ToInt32(costUsageSheet.Cells[i, 11].Value),         
+                                Vendor = Convert.ToString(costUsageSheet.Cells[i, 13].Value)       
+                            };
+
+                            entityData.DataRows.Add(rowData);
+                        }
+                    }
+
+                    if (entityData.DataRows.Count > 0)
+                    {
+                        matchedDataList.Add(entityData);
+                    }
+                }
+
+
+                //foreach (YourEntityClass entityData in matchedDataList)
+                //{
+                //    Console.WriteLine($"Account Number: {entityData.AccountNumber}");
+
+                //    foreach (EntityDataRow rowData in entityData.DataRows)
+                //    {
+                //        Console.WriteLine($"Entity: {rowData.Entity}");
+                //        Console.WriteLine($"Start Date: {rowData.StartDate}");
+                //        Console.WriteLine($"End Date: {rowData.EndDate}");
+                //        Console.WriteLine($"Bill Date: {rowData.BillDate}");
+                //        Console.WriteLine($"Cost: {rowData.Cost}");
+                //        Console.WriteLine($"Usage: {rowData.Usage}");
+                //        Console.WriteLine($"Vendor: {rowData.Vendor}");
+                //        Console.WriteLine("--------------------------------");
+                //    }
+
+                //    Console.WriteLine("================================");
+                //}
+
+                Worksheet utilitySheet = utilityTrackerWorkbook.Worksheets["Utility Data"];
+
+                int row = 2; 
+
+                foreach (YourEntityClass entityData in matchedDataList)
+                {
+                    utilitySheet.Cells[row, 1].Value = entityData.AccountNumber;
+
+                    foreach (EntityDataRow rowData in entityData.DataRows)
+                    {
+                        utilitySheet.Cells[row, 2].Value = rowData.Vendor;
+                        utilitySheet.Cells[row, 3].Value = rowData.Entity;
+                        utilitySheet.Cells[row, 4].Value = rowData.StartDate;
+                        utilitySheet.Cells[row, 5].Value = rowData.EndDate;
+                        utilitySheet.Cells[row, 6].Value = rowData.BillDate;
+                        utilitySheet.Cells[row, 7].Value = rowData.Cost;
+                        utilitySheet.Cells[row, 8].Value = rowData.Usage;
+
+                        row++;
+                    }
+
+                    row++;
+                }
+
+                utilitySheet.Columns.AutoFit();
+
+                //Worksheet utilitySheet = utilityTrackerWorkbook.Worksheets["Utility Data"];
+
+                int utilityLastRow = utilitySheet.Cells[utilitySheet.Rows.Count, 2].End[Excel.XlDirection.xlUp].Row + 1;
+
+
+                for (int i = 2; i < utilityLastRow; i++)
+                {
+                    string vendor = utilitySheet.Cells[i, 2].Value;
+                    string entity = utilitySheet.Cells[i, 3].Value;
+                    DateTime? startdate = utilitySheet.Cells[i, 4].Value;
+                    DateTime? enddate = utilitySheet.Cells[i, 5].Value;
+                    DateTime? billdate = utilitySheet.Cells[i, 6].Value;
+                    decimal cost = Convert.ToDecimal(utilitySheet.Cells[i, 7].Value);
+                    int usage = Convert.ToInt32(utilitySheet.Cells[i, 8].Value);
+
+                    int checkRow = i;
+                    while(vendor != null)
+                    {
+                        checkRow++;
+                        string duplicateVendor = utilitySheet.Cells[checkRow, 2].Value;
+                        string duplicateentity = utilitySheet.Cells[checkRow, 3].Value;
+                        DateTime? duplicatestartdate = utilitySheet.Cells[checkRow, 4].Value;
+                        DateTime? duplicateenddate = utilitySheet.Cells[checkRow, 5].Value;
+                        DateTime? duplicatebilldate = utilitySheet.Cells[checkRow, 6].Value;
+                        decimal duplicatecost = Convert.ToDecimal(utilitySheet.Cells[checkRow, 7].Value);
+                        int duplicateusage = Convert.ToInt32(utilitySheet.Cells[checkRow, 8].Value);
+
+                        if (duplicateVendor == null )
+                        {
+
+                            break;
+
+                        }
+                        if (vendor.Equals(duplicateVendor) && entity.Equals(duplicateentity) && startdate.Equals(duplicatestartdate) && enddate.Equals(duplicateenddate) && billdate.Equals(duplicatebilldate))
+                        {
+
+                            cost = cost + duplicatecost;
+                            usage = usage + duplicateusage;
+
+                            utilitySheet.Cells[i, 7].Value = cost;
+                            utilitySheet.Cells[i, 8].Value = usage;
+
+                            utilitySheet.Rows[checkRow].Delete(Excel.XlDeleteShiftDirection.xlShiftUp);
+                            checkRow--;
+                            utilityLastRow--;
+
+                        }
+                        
+
+                    }
+
+
+                }
+
+                
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message); ;
+            }
+            finally
+            {
+                excelApp.Quit();
+                Marshal.ReleaseComObject(excelApp);
+            }
+
+        }
+    }
+}
+
